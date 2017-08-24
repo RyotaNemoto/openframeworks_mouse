57aee20a4aadd56ac661e0a0674c0fce126c749a
Ryota Nemoto <git7kairo@gmail.com>
2017-08-24 16:54:28 +0900

    計算などに説明つけました

--- src/circle.cpp
+++ src/circle.cpp
@@ -11,34 +11,24 @@
 circle::circle(){
     
     
-    m=5;
-    M=10;
-    G=0.0667408;
+    m=5;//粒子の重さ
+    M=10;//マウスの重さ
+    G=0.0667408;//万有引力定数
     
     for(int i=0;i<NUM;i++){
 
         speed_[i]=ofVec3f(0,0,0);
-        /*
-        red[i]=ofRandom(255);
-        green[i]=ofRandom(255);
-        blue[i]=ofRandom(255);
-        */
-       
-        mVerts[i].set(ofRandom(-1*ofGetWidth()/2,ofGetWidth()/2),ofRandom(-1*ofGetHeight()/2,ofGetHeight()/2),0.0f);
-            //ofFloatColor color;
-            //color.setHsb(ofRandom(0.2),1,1);
-            //mColor[i].set(color);
         
-        //}
+        mVerts[i].set(ofRandom(-1*ofGetWidth()/2,ofGetWidth()/2),ofRandom(-1*ofGetHeight()/2,ofGetHeight()/2),0.0f);
+            ofFloatColor color;
+            color.setHsb(ofRandom(0.2),1,1);
+            mColor[i].set(color);
+        
+        
     }
         mVbo.setVertexData(mVerts, NUM, GL_DYNAMIC_DRAW);
         mVbo.setColorData(mColor,NUM,GL_DYNAMIC_DRAW);
-        //mVbo.setNormalData(mNormals, NUM, GL_DYNAMIC_DRAW);
-        
-
-        
-    
-
+        //mVbo.setNormalData(mNormals, NUM, GL_DYNAMIC_DRAW);//もしかしたら使うかも
     
 }
 void circle::update(){
@@ -47,12 +37,12 @@ void circle::update(){
 
     for(int i=0;i<NUM;i++){
         
-        pos_cm[i]=pos_m-mVerts[i];
-        F[i]=G*M*m/pos_cm[i].length()*pos_cm[i].length();
-        pos_cm[i]= pos_cm[i].normalize();
-        acc[i]=pos_cm[i]*F[i]/m;
-        speed_[i] += acc[i];
-        //cout<<acc[i]<<endl;
+        pos_cm[i]=pos_m-mVerts[i];//粒子とマウスの距離
+        F[i]=G*M*m/pos_cm[i].length()*pos_cm[i].length();//ばんゆういんりょくのけいさんしき
+        pos_cm[i]= pos_cm[i].normalize();//pos_cmを単位ベクトルにする
+        acc[i]=pos_cm[i]*F[i]/m;//加速度を求める
+        speed_[i] += acc[i];//速度を求める
+        //速度を制御する↓
         if(speed_[i].x>15){
             speed_[i].x=15;
             
@@ -70,11 +60,7 @@ void circle::update(){
         }
         
         
-        mVerts[i] += speed_[i];
-        //cout<<speed_[i]<<endl;
-        //mVerts[i] += ofRandom(1);
-        //cout << mVerts[i] << endl;
-      
+        mVerts[i] += speed_[i];//円の動き
         r=ofNoise(ofGetElapsedTimef()/i*2)*255;
         g=ofNoise(ofGetElapsedTimef()/i*3)*255;
         b=ofNoise(ofGetElapsedTimef()/i*5)*255;
