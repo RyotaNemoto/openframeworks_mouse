ff6f74887cda31feaa0655f30f8bee8c45c10b66
Kenju Wagatsuma <ken901waga@gmail.com>
2017-08-24 18:39:13 +0900

    使用されていないイベントリスナーを削除しました

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -13,64 +13,6 @@ void ofApp::update(){
     c.update();
 }
 
-
 void ofApp::draw(){
     c.draw();
-}
-
-void ofApp::keyPressed(int key){
-    
-}
-
-void ofApp::keyReleased(int key){
-    
-}
-
-
-void ofApp::mouseMoved(int x, int y ){
-    
-}
-
-
-void ofApp::mouseDragged(int x, int y, int button){
-    
-    
-}
-
-
-void ofApp::mousePressed(int x, int y, int button){
-    //     mouse_pressed=true;
-}
-
-
-void ofApp::mouseReleased(int x, int y, int button){
-    //    mouse_pressed=false;
-    //    for(int i=0; i<NUM; i++){
-    //        speed_[i]=ofVec2f(ofRandom(-5,5),ofRandom(-5,5));
-    //    }
-}
-
-
-void ofApp::mouseEntered(int x, int y){
-    
-}
-
-
-void ofApp::mouseExited(int x, int y){
-    
-}
-
-
-void ofApp::windowResized(int w, int h){
-    
-}
-
-
-void ofApp::gotMessage(ofMessage msg){
-    
-}
-
-
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-    
 }
